8f9a61a9c406e6d42c45699788f83b1114f27a04
Wilson Dos Santos <wilson-senx@hotmail.com>
2022-06-02 21:06:01 -0300

    :sparkles: Ignite Feed vesrion 1.0.1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Post } from './components/Post';
+
+export function App() {
+
+  return (
+    <div className="App">
+      <Post 
+        author="wilson santos"
+        content="
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem perferendis optio nisi reiciendis aperiam sunt eveniet, laudantium quas facere. Minus odio nemo veritatis incidunt omnis esse debitis. Id, iusto expedita."
+      />
+    </div>
+  )
+}
