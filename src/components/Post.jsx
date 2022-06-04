9e6d0b55ea954c0f510c62639c5689d285292873
Wilson Dos Santos <wilson-senx@hotmail.com>
2022-06-03 23:49:31 -0300

    :fire: Add functionality to user comment a post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,40 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import { Comment } from './Comment';
 import { Avatar } from './Avatar';
 
 import styles from './Post.module.css';
 
-export function Post(props) {
+export function Post({ author, publishedAt, content }) {
+
+  const [comments, setComments] = useState([]);
+
+  const publishedDateFormat = format(publishedAt, "d 'de' LLLL 'às' HH:mm", {
+    locale: ptBR,
+  });
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true
+  });
+
+  function handleCreateNewComment(event) {
+    event.preventDefault();
+    console.log('oi')
+
+    setComments();
+  }
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar hasBorder={true} src="https://github.com/WGS-94.png" />
+          <Avatar hasBorder={true} src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Wilso Dos Santos</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title='11 de Maio às 08:13' dateTime='2022-05-11 08:13:38'>
-          publicado há 1h
+        <time title={publishedDateFormat} dateTime={publishedAt.toISOString()}>
+          {publishedDateRelativeToNow}
         </time>
 
       </header>
 
       <div className={styles.contentPost}>
-        <p>Fala galeraa 👋</p>
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-        <p>👉 <a href="#">wilson.design/doctorcare</a></p>
-        <p>
-          <a href="#">#novoprojeto</a>{' '}
-          <a href="#">#nlw</a>{' '}
-          <a href="#">#rocketseat</a>
-          <a href="#">#nafau</a>
-        </p>
+
+        {content.map((line) => {
+          if(line.type === 'paragraph') {
+            return <p>{line.content}</p>
+          }else if (line.type === 'link') {
+            return <p>👉 <a href="#">{line.content}</a></p>
+          }
+        })}
+        
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
         <textarea placeholder='Deixe um comentário' />
         <footer>
